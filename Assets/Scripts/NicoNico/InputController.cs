1d564f28df7586f4c4faf34fd221b16671908916
fumito-ikemoto <fumito-ikemoto@united.jp>
2019-05-07 16:05:51 +0900

    ニコニコのコメントみたいなやつつくるその1

---
+++ Assets/Scripts/NicoNico/InputController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class InputController : MonoBehaviour {
+
+    public InputField inputField;
+
+    /// <summary>
+    /// 入力完了時にコール
+    /// </summary>
+    public void onEndEdit()
+    {
+
+        GameObject obj = PhotonNetwork.Instantiate("Message", Vector3.zero, Quaternion.identity, 0);
+
+        //初期位置を適当に設定
+        float x = 380f;
+        float y = Random.Range(-150f, 150f);
+        Vector3 pos = new Vector3(x, y, 0);
+        obj.GetComponent<RectTransform>().localPosition = pos;
+
+        //入力されているテキストをセット
+        
+        
+
+        Debug.Log("入力完了");
+    }
+}
---
+++ Assets/Scripts/NicoNico/Network.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+namespace Scripts.NicoNico
+{
+    public class Network : MonoBehaviourPunCallbacks
+    {
+
+        private string gameVersion = "1";
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            Connect();
+        }
+
+        private void Connect()
+        {
+            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+            if (PhotonNetwork.IsConnected)
+            {
+                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                // #Critical, we must first and foremost connect to Photon Online Server.
+                PhotonNetwork.GameVersion = gameVersion;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("マスターに接続");
+
+            //ロビーに入る
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            Debug.Log("ロビーに入りました");
+
+            //ルームに入室する
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("ルームへ入室しました");
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("ルームの入室に失敗しました。");
+
+            // ルームがないと入室に失敗するため、ルームがない場合は自分で作る
+            PhotonNetwork.CreateRoom("myRoomName");
+        }
+
+    }
+}
